782c1c9769a9cacbe406b474f39ad6fad689dace
shining_sir <shining_man@163.com>
2017-03-06 16:23:04 +0800

    Confirm

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    // singleton
+    public static GameController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
